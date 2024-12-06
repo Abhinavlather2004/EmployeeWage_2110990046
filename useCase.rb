a65e2bb7258ab16555d99339c59ac28fd0053bfa
Abhinavlather2004 <latherabhinav55@gmail.com>
2024-12-06 11:01:49 +0530

    [Abhinav]Add : uc4 added

--- useCase.rb
+++ useCase.rb
@@ -11,26 +11,33 @@ end
 
 # check_attendance()
 
-# def calculate_daily_wage(wage_per_hour, full_time_hour)
-#   one_day_wage = wage_per_hour * full_time_hour
-#   return one_day_wage
-# end
+def calculate_full_time_wage(wage_per_hour, full_time_hour)
+  one_day_wage = wage_per_hour * full_time_hour
+  return one_day_wage
+end
+
 def calculate_part_time_wage(wage_per_hour, part_time_hour)
   wage_per_hour * part_time_hour
 end
 
 wage_per_hour = 20
-# full_time_hour = 8
+full_time_hour = 8
 part_time_hour = 4
 
 # Check attendance and calculate wage only if employee is present
 if check_attendance 
   
-  # daily_wage = calculate_daily_wage(wage_per_hour, full_time_hour)
-  # puts "One day wage is: $#{daily_wage}"
+  work_type = rand(2)
+  if work_type == 1
+  daily_wage = calculate_full_time_wage(wage_per_hour, full_time_hour)
+  puts "One day full-time wage is: $#{daily_wage}"
   
+  else
   daily_wage = calculate_part_time_wage(wage_per_hour, part_time_hour)
    puts "One day part-time wage is: $#{daily_wage}"
 
+  end
+else
+  puts "No wage as the employee is absent."
 end
 
